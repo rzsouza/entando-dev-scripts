78fef375a7d8c924c13d59b0121f0e178f1e9206
Rodrigo Ziviani de Souza <rodrigo.ziviani@gmail.com>
2018-11-01 07:13:24 -0300

    Few more scripts and bug fixes

--- entandoAliases.sh
+++ entandoAliases.sh
@@ -2,15 +2,22 @@
 
 export ENTANDO_HOME=~/Entando
 
+alias s='source ~/.zshrc'
+alias z='$EDITOR ~/.zshrc'
+alias G='| grep'
+alias AG='alias | grep'
+
 alias gp='git pull'
 alias gco='git checkout'
 alias gcm='git checkout master'
+alias gst='git status'
+
 alias e='cd $ENTANDO_HOME'
 alias es='cd ~/bin/entando-dev-scripts'
 alias eb='cd $ENTANDO_HOME/BE'
-alias ec='cd $ENTANDO_HOME/BE/entando-core'
-alias ep='cd $ENTANDO_HOME/BE/entando-components'
-alias ea='cd $ENTANDO_HOME/BE/entando-archetypes'
+alias ec='cd $ENTANDO_HOME/BE/entando-core && pwd'
+alias ep='cd $ENTANDO_HOME/BE/entando-components && pwd'
+alias ea='cd $ENTANDO_HOME/BE/entando-archetypes && pwd'
 alias ab='cd $ENTANDO_HOME/app-builder'
 alias j='mvn clean jetty:run'
 alias jd='(export MAVEN_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=5005,server=y,suspend=n"; j)'
@@ -21,6 +28,7 @@ alias acm='ec && gcm && ep && gcm && ea && gcm'
 alias ac5='ec && gco v5.0.1-dev && ep && gco v5.0.1-dev && ea && gco v5.0.1-dev'
 alias gpa='ec && gp && ep && gp && ea && gp'
 alias gpai='ec && gp && i && ep && gp && i && ea && gp && i'
+alias agst='(ec && gst && ep && gst && ea && gst)'
 alias n='notify-send'
 alias ap='cd $ENTANDO_HOME/myapp' 
 alias ap5='cd $ENTANDO_HOME/myapp501' 
